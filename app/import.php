cbccbf052ea572072a1a3a672e2831e86008b920
Randika Srimal <email2randika@gmail.com>
2021-09-29 20:48:59 +0530

    Create initial composer setup

---
+++ app/import.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Area51projects\Psql2mysql\Importer;
+
+echo Importer::sayHelloWorld();
---
+++ app/src/Importer.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Area51projects\Psql2mysql;
+
+class Importer {
+  public static function sayHelloWorld() {
+    return 'Hello World';
+  }
+}
