2b06ffcaaf96134156739b5745341fa342eaab3a
Randika Srimal <email2randika@gmail.com>
2021-09-29 23:14:57 +0530

    Setup requirements for PostgreSQL connection

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,16 @@
 FROM php:7.4-fpm-alpine
 
+RUN set -ex \
+  && apk --no-cache add \
+    postgresql-dev
+
 ARG UID
 
 WORKDIR /var/www/html
 
 RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 
-RUN docker-php-ext-install pdo_mysql
+RUN docker-php-ext-install pdo_mysql pdo_pgsql
 
 # Install PHP extensions end
 
--- app/app.php
+++ app/app.php
@@ -1,17 +1,28 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
 use Medoo\Medoo;
 
-$mySqlDatabase = new Medoo([
-	'type' => 'mysql',
-	'host' => '127.0.0.1',
-	'database' => 'levantine_hill',
-	'username' => 'root',
-	'password' => 'root',
-	'port' => 3306,
+$postgreConnection = new Medoo([
+	'type' => 'postgresql',
+	'host' => $_ENV['POSTGRESQL_HOST'],
+	'database' => $_ENV['POSTGRESQL_DATABASE'],
+	'username' => $_ENV['POSTGRESQL_USER'],
+	'password' => $_ENV['POSTGRESQL_PASSWORD'],
+	'port' => $_ENV['POSTGRESQL_PORT'],
 ]);
 
-$data = $mySqlDatabase->select('users', '*');
+$mySqlConnection = new Medoo([
+	'type' => 'mysql',
+	'host' => $_ENV['MYSQL_HOST'],
+	'database' => $_ENV['MYSQL_DATABASE'],
+	'username' => $_ENV['MYSQL_USER'],
+	'password' => $_ENV['MYSQL_PASSWORD'],
+	'port' => $_ENV['MYSQL_PORT'],
+]);
+
+$data = $postgreConnection->select('users', '*');
  
 echo json_encode($data);
